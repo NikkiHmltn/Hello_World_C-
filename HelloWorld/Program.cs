88ebde191c17b22f9c7f0b50b95f098cb35d1d19
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-17 11:24:47 -0800

    practice with data types starting with sbyte to int

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,14 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int age = 27; //defines variable as an integer, stores whole num from -2,147,483,648 to 2,147,483,648
+            sbyte x = 120; //sbyte means short byte and are whole numbers from -128 to 127
+            short y = 30000; // whole numbers from -32767 to 32767
+            long z = 9000000000000000; //whole numbers from –9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+            Console.WriteLine(age);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
         }
     }
 }
